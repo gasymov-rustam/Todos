f7ada795fc91f9cc5b0721d45cd2e1b42aa06800
rust_man <gasymovrustam86@yandex.com>
2021-11-16 20:43:55 +0200

    changed some names

--- src/hooks/useTodos.jsx
+++ src/hooks/useTodos.jsx
@@ -2,38 +2,38 @@ import { useEffect, useContext, useMemo, useReducer, createContext } from "react
 import { getTodos } from "../api/todos";
 
 const initialState = [];
-const TododsContext = createContext(initialState);
-export const useTodos = () => useContext(TododsContext);
+const TodosContext = createContext(initialState);
+export const useTodos = () => useContext(TodosContext);
 export const TodosProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const memoValue = useMemo(() => [state, dispatch], [state]);
-  useEffect(() => {
-    (async function () {
-      const [getTodo, getTodoError] = await getTodos();
-      if (!getTodoError) dispatch({ type: "INIT", payload: getTodo });
-    })();
-  }, []);
-  return <TododsContext.Provider value={memoValue}>{children}</TododsContext.Provider>;
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const memoValue = useMemo(() => [state, dispatch], [state]);
+    useEffect(() => {
+        (async function () {
+            const [getTodo, getTodoError] = await getTodos();
+            if (!getTodoError) dispatch({ type: "INIT", payload: getTodo });
+        })();
+    }, []);
+    return <TodosContext.Provider value={memoValue}>{children}</TodosContext.Provider>;
 };
 
 function reducer(state, { type, payload }) {
-  switch (type) {
-    case "INIT": {
-      return payload;
+    switch (type) {
+        case "INIT": {
+            return payload;
+        }
+        case "ADD": {
+            return [...state, payload];
+        }
+        case "NEXT_STATUS": {
+            const newState = [...state];
+            const todoIdx = newState.findIndex((todo) => todo.id === payload.id);
+            newState.splice(todoIdx, 1, payload);
+            return newState;
+        }
+        case "DELETE": {
+            return [...state].filter((todo) => todo.id !== payload);
+        }
+        default:
+            throw new Error(`Unknown type ===>>>> ${type}`);
     }
-    case "ADD": {
-      return [...state, payload];
-    }
-    case "NEXT_STATUS": {
-      const newState = [...state];
-      const todoIdx = newState.findIndex((todo) => todo.id === payload.id);
-      newState.splice(todoIdx, 1, payload);
-      return newState;
-    }
-    case "DELETE": {
-      return [...state].filter((todo) => todo.id !== payload);
-    }
-    default:
-      throw new Error(`Unknown type ===>>>> ${type}`);
-  }
 }
